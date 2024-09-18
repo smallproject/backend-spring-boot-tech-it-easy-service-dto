8199591dd045e808e3db464d71003c22b6960132
smallproject <j.a.villas@live.com.ph>
2024-09-18 09:28:48 +0200

    added data sql script for data seeding

---
+++ tech-it-easy/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.televisions(
+    current_stock, hdr, height, price, smart_tv, sold, voice_control, width, wifi, local_date, brand, name, screen_quality, screen_type, type)
+VALUES ('10', 'true', '71.0' , '399', 'true', '2', 'true', '123', 'true', '2024-01-01', 'Hisense', '55A72KQ (2023)', '', 'Ultra HD 4K','Qled-tv');

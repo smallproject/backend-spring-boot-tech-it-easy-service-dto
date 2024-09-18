32f30fb6708f564639c9c844d375c01aec9d3ef8
smallproject <j.a.villas@live.com.ph>
2024-09-18 21:07:45 +0200

    Added global exception handler

--- tech-it-easy/src/main/java/nl/smallproject/www/techiteasy/controllers/ExceptionsController.java
+++ tech-it-easy/src/main/java/nl/smallproject/www/techiteasy/controllers/ExceptionsController.java
@@ -1,10 +1,16 @@
 package nl.smallproject.www.techiteasy.controllers;
 
 import nl.smallproject.www.techiteasy.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionsController {
@@ -12,5 +18,12 @@ public class ExceptionsController {
     @ExceptionHandler(value = RecordNotFoundException.class)
     public ResponseEntity<Object> exceptions(RecordNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+
+        return new ResponseEntity<>(errors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
